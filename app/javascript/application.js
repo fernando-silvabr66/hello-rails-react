d550b29e90e44ebc3e240beeed4261245d7f1534
Fernando Silva <fernando.silvabr66@gmail.com>
2023-01-24 10:19:58 -0300

    Change App to arrow function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
+// function App() {
+//   return (<h1>Hello World!</h1>);
+// }
+
+const App = () => {
   return (<h1>Hello World!</h1>);
 }
 
