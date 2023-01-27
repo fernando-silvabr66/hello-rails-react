2d1c02edebb7dbebc9b65254d9f4742236431aa0
Fernando Silva <fernando.silvabr66@gmail.com>
2023-01-27 18:23:53 -0300

    Update application with Reducer, Store and refactor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import Greeting from './components/Greeting';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { fetchGreeting } from './redux/greetings/greetingsSlice';
 
-// function App() {
-//   return (<h1>Hello World!</h1>);
-// }
+store.dispatch(fetchGreeting());
 
-const App = () => {
-  return (<h1>Hello World!</h1>);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Greeting />} />
+      </Routes>
+    </Router>
+  );
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
